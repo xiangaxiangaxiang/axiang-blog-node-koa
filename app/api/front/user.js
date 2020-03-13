35f8bd0e11a7f7ec9c0788470e0a44176dc557f8
xiangaxiangaxiang <872632899@qq.com>
2020-03-13 22:17:24 +0800

    test upload file

--- app/api/front/user.js
+++ app/api/front/user.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router')
+const Client = require('ftp');
+const fs = require('fs');
 
 const {User} = require('@models/user')
 const {RegisterValidator} = require('@validator')
@@ -7,9 +9,27 @@ const router = new Router({
     prefix: '/front/user'
 })
 
+const c = new Client();
+
 router.post('/test', async (ctx) => {
-    const file = ctx.request.body.files
-    console.log(`file is ${file}`)
+    const files = ctx.request.body.file
+    let filesArr = Array.from(files)
+    let file = filesArr[0]
+    // console.log(`file is ${file[0]}`)
+    c.connect({
+        user: 'cpx',
+        password: 'cpxlalala'
+    })
+    c.on('ready', function() {
+        c.put(file, '1.png',function(err) {
+            if (err) throw err;
+            c.end();
+        });
+    });
+    ctx.body = {
+        status: 0,
+        msg: 'success'
+    }
 })
 
 router.post('/register', async (ctx) => {
