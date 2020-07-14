b4a0387089b0a7bdf0458ed2d0c15e2f00cee3af
xiangaxiangaxiang <872632899@qq.com>
2020-07-14 20:22:13 +0800

    修改用户字段

--- app/models/user.js
+++ app/models/user.js
@@ -84,6 +84,12 @@ class User extends Model {
         if (!user) {
             throw new global.errs.NotFound('账号不存在')
         }
+        if (password) {
+            const correct = bcrypt.compareSync(password, user.password)
+            if (!correct) {
+                throw new global.errs.AuthFailed('密码不正确')
+            }
+        }
         const userInfo = {
             nickname: nickname ? nickname : user.nickname,
             password: password ? password: user.password,
