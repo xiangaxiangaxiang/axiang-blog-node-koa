51e173e3764791bc8aa71069d8e80484183f3ce7
xiangaxiangaxiang <872632899@qq.com>
2020-09-08 20:12:13 +0800

    ftp上传文件备份

---
+++ app/lib/upload-ftp.js
@@ -0,0 +1,29 @@
+// ftp 方式上传文件
+const Client = require('ftp');
+const fs = require('fs');
+
+const ftp = new Client();
+
+// 上传文件到ftp服务器
+function upload(pathlist) {
+    ftp.connect(global.config.ftp)
+
+    ftp.on('ready', function() {
+        for (let i in pathlist) {
+            ftp.put(pathlist[i].filePath, pathlist[i].savePath,function(err) {
+                if (err) {
+                    console.log('ftp error')
+                    console.log(err)
+                    ftp.end();
+                    throw err
+                }
+                console.log(`file path is ${pathlist[i].filePath}, save path is ${pathlist[i].savePath}`)
+            });
+        }
+        ftp.end()
+    });
+}
+
+module.exports = {
+    upload
+}
